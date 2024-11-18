b26eb9b0cb2242bea39ce9e623645628be1d194b
Adam Buckingham <AJBuckingham7@gmail.com>
2024-11-18 15:52:43 -0500

    Check tag value, and update format based

--- daily_god/apps/frontend/templatetags/_extras.py
+++ daily_god/apps/frontend/templatetags/_extras.py
@@ -1,8 +1,12 @@
 from django import template
+from taggit.models import TagBase, GenericTaggedItemBase
 
 register = template.Library()
 
 @register.filter
 def class_name(obj):
-    print(obj.__class__.__name__)
     return obj.__class__.__name__
+
+@register.filter
+def check_tag(tag_name, obj):
+    return tag_name in [tag.name for tag in obj.tags.all()]
