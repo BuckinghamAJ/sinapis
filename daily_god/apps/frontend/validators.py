81dd1742bad0ec6359148fc89999bc1ec45dfb29
Adam Buckingham <adamjbuckingham7@gmail.com>
2025-02-28 21:49:32 -0500

    Updates

--- daily_god/apps/frontend/validators.py
+++ daily_god/apps/frontend/validators.py
@@ -3,17 +3,28 @@ from profanity.extras import ProfanityFilter
 from django.core.exceptions import ValidationError
 import logging
 logger = logging.getLogger('app')
+import os
 
-pf = ProfanityFilter()
+word_list = Path(os.path.abspath(os.path.dirname(__file__)), 'config', 'wordlist.txt')
+censor_list = None
+with open(word_list, 'r') as f:
+    censor_list = [line.strip() for line in f.readlines()]
+
+pf = ProfanityFilter(custom_censor_list=censor_list)
 
 class SeedProfanityFilter(ProfanityFilter):
     def __init__(self, word_list: Path = None, **kwargs):
         super().__init__(**kwargs)
         self._words_file = word_list if word_list else self._words_file
 
-spf = SeedProfanityFilter(word_list=Path(Path(__file__).parent, 'config', 'wordlist.txt'))
+
+
+spf = SeedProfanityFilter(word_list=Path(os.path.abspath(os.path.dirname(__file__)), 'config', 'wordlist.txt'))
+
 
 def validate_is_profane(value):
     if pf.is_profane(value) is True:
-        logger.debug(spf.censor(value))
+        bad_words = pf.get_profane_words()
+        logger.debug(bad_words)
+        logger.debug(pf.censor(value))
         raise ValidationError('Please remove any profanity/swear words.')
